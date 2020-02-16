b5164c9579a12baefff8a4b3dcafbccaa2cf1a82
David Saltares <david.saltares@gmail.com>
2020-02-16 17:06:16 +0200

    reorganise brain files

--- game/Enemies/Brain/BrainIdle.gd
+++
@@ -1,9 +0,0 @@
-extends BrainState
-class_name BrainIdle
-
-
-func physics_process(delta: float) -> void:
-	_parent.physics_process(delta)
-
-func enter(msg: = {}) -> void:
-	animation_player.play("idle")
