8a8e9acab3dcd0f4194417d1f7a8fbd5abc41178
Jay Cano <inaki.cano@gmail.com>
2020-02-17 14:18:40 +0000

    differentiate enemies by difficulty when spawning

--- game/Levels/EnemySpawn.gd
+++ game/Levels/EnemySpawn.gd
@@ -3,7 +3,9 @@ extends Node2D
 class_name EnemySpawn
 
 export(float, 0, 1) var enemy_probability = 1
-export(int, "Easy", "Medium", "Hard") var enemy_max_level
+
+enum DIFFICULTY {EASY, MEDIUM, HARD}
+export(DIFFICULTY) var enemy_max_level = DIFFICULTY.EASY
 
 const ENEMIES = [
 	"res://Enemies/Alien/Alien.tscn",
@@ -14,14 +16,14 @@ const ENEMIES = [
 func _ready():
 	# Calculate a coefficient that increases with distance
 	var distance = get_parent().distance
-	var coef = 1 + (distance / 100.0)
+	var coef = 1 + (distance / 50.0)
 	
 	# Roll a number to spawn an enemy
 	randomize()
 	var roll = randf()
 	
 	if roll < coef * enemy_probability:
-		var enemy_scene = ResourceLoader.load(ENEMIES[randi() % 3])
+		var enemy_scene = ResourceLoader.load(ENEMIES[randi() % (enemy_max_level + 1)])
 		var enemy = enemy_scene.instance()
 		
 		add_child(enemy)
