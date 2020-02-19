0a586890c4ef8887224dd51cb1fbfb57f755a221
David Saltares <david.saltares@gmail.com>
2020-02-19 08:26:19 +0200

    add soundtrack scene with 1 track

--- assets/ASSETS.md
+++ assets/ASSETS.md
@@ -2,3 +2,4 @@
 
 * [The Essential Retro Video Game Sound Effects Collection](https://opengameart.org/content/512-sound-effects-8-bit-style) by [SubSpaceAudio](https://opengameart.org/users/subspaceaudio)
 * [Roguenoir Cyberpunk Assetpack](https://s4m-ur4i.itch.io/roguenoir-cyberpunk-assetpack-pixelart) by [Sam](https://s4m-ur4i.itch.io/)
+* [Dark Retrowave Cyberpunk](https://www.jamendo.com/track/1655535/dark-retrowave-cyberpunk) by [Alex Zavesa](https://www.jamendo.com/artist/486413/alexzavesa)s
---
+++ game/Music/SoundTrack.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var current_track : AudioStreamPlayer
+var current_track_idx := -1
+
+func _process(delta: float) -> void:
+	if not current_track or not current_track.playing:
+		next()
+	
+
+func next() -> void:	
+	if get_child_count() == 0:
+		return
+	
+	current_track_idx = (current_track_idx + 1) % get_child_count()
+	current_track = get_child(current_track_idx) as AudioStreamPlayer
+	current_track.play()
+	var track : AudioStreamPlayer = get_child(current_track) as AudioStreamPlayer
+	if not track.playing:
+		next()
