2f5a423b420d1e80a17818a448207261c9e6e03f
Jay Cano <inaki.cano@gmail.com>
2020-02-23 15:13:12 +0000

    add more levels

--- game/Globals.gd
+++ game/Globals.gd
@@ -21,19 +21,34 @@ var LEVELS = {
 		"exit_height": LEVEL_HEIGHT.LOW
 	},
 	"res://Levels/level5.tscn": {
+		"entrance_height": LEVEL_HEIGHT.LOW,
+		"exit_height": LEVEL_HEIGHT.HIGH
+	},
+	"res://Levels/level6.tscn": {
 		"entrance_height": LEVEL_HEIGHT.HIGH,
 		"exit_height": LEVEL_HEIGHT.HIGH
+	},
+	"res://Levels/level7.tscn": {
+		"entrance_height": LEVEL_HEIGHT.LOW,
+		"exit_height": LEVEL_HEIGHT.LOW
+	},
+	"res://Levels/level8.tscn": {
+		"entrance_height": LEVEL_HEIGHT.HIGH,
+		"exit_height": LEVEL_HEIGHT.LOW
 	}
 }
 
 var LEVELS_BY_ENTRANCE_HEIGHT = {
 	LEVEL_HEIGHT.LOW: [
 		"res://Levels/level2.tscn",
-		"res://Levels/level4.tscn"
+		"res://Levels/level4.tscn",
+		"res://Levels/level5.tscn",
+		"res://Levels/level7.tscn"
 	],
 	LEVEL_HEIGHT.HIGH: [
 		"res://Levels/level1.tscn",
 		"res://Levels/level3.tscn",
-		"res://Levels/level5.tscn"
+		"res://Levels/level6.tscn",
+		"res://Levels/level8.tscn"
 	]
 }
