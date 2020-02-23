8070950cf5fb45b009a0e53a56858aa05d81b496
Jay Cano <inaki.cano@gmail.com>
2020-02-23 12:06:45 +0000

    add some more interesting levels

--- game/Levels/InfiniteLevelManager.gd
+++ game/Levels/InfiniteLevelManager.gd
@@ -26,7 +26,7 @@ func _ready():
 func load_random_level(height = null) -> Node2D:
 	randomize()
 	
-	var levels = Globals.LEVELS_BY_ENTRANCE_HEIGHT[height] if height else Globals.LEVELS.keys()
+	var levels = Globals.LEVELS_BY_ENTRANCE_HEIGHT[height] if height != null else Globals.LEVELS.keys()
 	
 	var scene = ResourceLoader.load(levels[randi() % len(levels)])
 	var level = scene.instance()
@@ -44,6 +44,11 @@ func spawn_player(level: Level):
 func append_new_level():
 	# Get an instance of a new random level
 	var level = load_random_level(Globals.LEVELS[current_level.filename].exit_height)
+	print("//////////////")
+	print(current_level.filename)
+	print(Globals.LEVELS[current_level.filename].exit_height)
+	print(level.filename)
+	print(Globals.LEVELS[level.filename].entrance_height)
 	
 	# Add info about how far the player has gone
 	level.distance = distance
